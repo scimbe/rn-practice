6182a873538305a3989f4bfffe11e524b9421cfc
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2024-02-29 22:51:22 +0100

    Update start-topo01.sh

--- topo01/start-topo01.sh
+++ topo01/start-topo01.sh
@@ -1,6 +1,6 @@
 echo "start topology"
 ./getIntWithIntenet.sh
-sudo mv /etc/resolv.conf /etc/resolv.conf.old
+sudo cp /etc/resolv.conf /etc/resolv.conf.old
 echo "nameserver 10.0.1.2" | sudo tee /etc/resolv.conf
 sudo bash -c  'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
 sudo mn -c

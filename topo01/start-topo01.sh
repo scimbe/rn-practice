3a12a1108bf0a27f62e9b5c61e06763d68e46094
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2024-04-15 13:28:25 +0200

    Update start-topo01.sh

--- topo01/start-topo01.sh
+++ topo01/start-topo01.sh
@@ -1,8 +1,21 @@
 echo "start topology"
 ./getIntWithIntenet.sh
-sudo cp /etc/resolv.conf /etc/resolv.conf.old
-echo "nameserver 10.0.1.2" | sudo tee /etc/resolv.conf
-sudo bash -c  'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
+
+resolv_conf="/etc/resolv.conf"
+sudo cp $resolv_conf /etc/resolv.conf.old
+
+# Nameserver, der hinzugefügt werden soll, falls nicht vorhanden
+nameserver="nameserver 10.0.1.2"
+
+# Überprüfen, ob der Nameserver bereits in der Datei vorhanden ist
+if ! grep -q "^$nameserver$" "$resolv_conf"; then
+    echo "Der Nameserver $nameserver wird hinzugefügt."
+    # Hinzufügen des Nameservers am Ende der Datei
+    echo "$nameserver" | sudo tee -a "$resolv_conf" > /dev/null
+else
+    echo "Der Nameserver $nameserver ist bereits in der Datei vorhanden."
+fi
+
 sudo mn -c
 sudo -E  python3 topo01.py
 sudo mn -c

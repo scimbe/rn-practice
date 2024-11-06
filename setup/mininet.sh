acd0caa991de81c78bb193a5a629e6556b154fd0
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2024-11-06 12:59:53 +0100

    Update mininet.sh

--- setup/mininet.sh
+++ setup/mininet.sh
@@ -1,4 +1,4 @@
 sudo apt update
-sudo apt -y install snap python3-pip dnsmasq whois nmap snapd curl arping dsniff frr iperf3 openvswitch-switch traceroute hping3
+sudo apt -y install psmisc snap python3-pip dnsmasq whois nmap snapd curl arping dsniff frr iperf3 openvswitch-switch traceroute hping3
 sudo snap install searchsploit
 pip3 install pytest

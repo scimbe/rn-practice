6aa82510d671f4030560a8ccfe33fd7319b62da3
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-01-03 17:07:32 +0100

    Update mininet.sh

--- setup/mininet.sh
+++ setup/mininet.sh
@@ -1,4 +1,7 @@
 sudo apt update
-sudo apt -y install psmisc snap python3-pip dnsmasq whois nmap snapd curl arping dsniff frr iperf3 openvswitch-switch traceroute hping3 wireshark iperf
-sudo snap install searchsploit
+sudo apt -y install psmisc  python3-pip dnsmasq whois nmap curl arping dsniff frr iperf3 openvswitch-switch traceroute hping3 wireshark iperf
+sudo git clone https://gitlab.com/exploit-database/exploitdb.git /opt/exploitdb
+sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
+cp /opt/exploitdb/.searchsploit_rc ~/
+export EDB_PATH="/opt/exploitdb"
 pip3 install pytest

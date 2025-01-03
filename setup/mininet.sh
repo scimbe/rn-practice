f2066c1294e44289f582d60ccd7eccc4a2ff7946
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-01-03 17:09:39 +0100

    Update mininet.sh

--- setup/mininet.sh
+++ setup/mininet.sh
@@ -4,4 +4,4 @@ sudo git clone https://gitlab.com/exploit-database/exploitdb.git /opt/exploitdb
 sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
 cp /opt/exploitdb/.searchsploit_rc ~/
 export EDB_PATH="/opt/exploitdb"
-pip3 install pytest
+sudo pip3 install pytest

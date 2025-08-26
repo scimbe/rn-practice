ba81482a764b4e7857f4f68454d3af75e15dcb98
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-08-26 17:56:20 +0200

    Implement start_ovs function for Open vSwitch

--- topoP03/start-topoP03.sh
+++ topoP03/start-topoP03.sh
@@ -1,4 +1,26 @@
+
+
+start_ovs() {
+    log "INFO" "Open vSwitch wird gestartet"
+    if ! sudo service openvswitch-switch status &>/dev/null; then
+        sudo service openvswitch-switch start || {
+            log "ERROR" "Open vSwitch konnte nicht gestartet werden."
+            exit 1
+        }
+    else
+        log "INFO" "Open vSwitch läuft bereits."
+    fi
+    sudo ovs-vsctl show &>/dev/null || {
+        log "ERROR" "Open vSwitch scheint nicht korrekt zu funktionieren."
+        exit 1
+    }
+}
+
+
+
+
 echo "start topology"
+start_ovs
 #./getIntWithIntenet.sh
 #sudo mv /etc/resolv.conf /etc/resolv.conf.old
 #echo "nameserver 10.0.1.2" | sudo tee /etc/resolv.conf
